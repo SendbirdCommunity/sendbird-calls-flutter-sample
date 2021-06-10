fbaa542a335a892071e005fd8c502c0d5de70894
supertopoz <deepsleepsounds@gmail.com>
2021-06-10 09:09:37 +0900

    Commit to add iOS init of Sendbird Calls from flutter

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,55 @@
 import UIKit
 import Flutter
+import SendBirdCalls
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    
+    
+    func initSendbird(args: Any, completion: @escaping (Bool) -> ()) {
+        let APP_ID = "885C2616-DBF8-4BDC-9178-4A1A662614E3"
+        SendBirdCall.configure(appId: APP_ID)
+        SBCLogger.setLoggerLevel(.info)
+        let params = AuthenticateParams(userId: "UserA", accessToken: "TOKEN")
+        
+        SendBirdCall.authenticate(with: params) { (user, error) in
+            
+            guard let user = user, error == nil else {
+                // Handle error.
+                print("Error")
+                completion(false)
+                return
+            }
+            completion(true)
+            print(user)
+        }
+    }
+    
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        
+        
+        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+        let callsChannel = FlutterMethodChannel(name: "com.sendbird.calls/method",
+                                                binaryMessenger: controller.binaryMessenger)
+        callsChannel.setMethodCallHandler({
+            //May Need @escaping for async.
+            (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
+            // Note: this method is invoked on the UI thread.
+            // Handle battery messages.
+            guard call.method == "init" else { return }
+            if let args = call.arguments {
+                print(args)
+                self.initSendbird(args: args) { (connected) -> () in
+                    result(connected)
+                }
+            }
+        })
+        
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
+
